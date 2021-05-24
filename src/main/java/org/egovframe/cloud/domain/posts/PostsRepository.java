b8523d1f87c4dd35d492c559c5c88d5ea27d5d57
kimjaeyeol <godbasic97@gmail.com>
2021-05-24 16:50:27 +0900

    화면단 요청 처리 작업

--- src/main/java/org/egovframe/cloud/domain/posts/PostsRepository.java
+++ src/main/java/org/egovframe/cloud/domain/posts/PostsRepository.java
@@ -1,9 +1,16 @@
 package org.egovframe.cloud.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository<Entity클래스, PK타입> 를 상속하면 기본적인 CRUD 메소드가 자동 생성된다.
 // @Repository 를 추가할 필요도 없음
 // Entity 클래스와 기본 Repository는 밀접한 관계이므로 도메인 패키지에서 함께 관리한다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // Spring Data Jpa 에서 제공하지 않는 메소드는 이렇게 쿼리로 작성할 수 있다
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/egovframe/cloud/service/PostsService.java
+++ src/main/java/org/egovframe/cloud/service/PostsService.java
@@ -3,11 +3,15 @@ package org.egovframe.cloud.service;
 import lombok.RequiredArgsConstructor;
 import org.egovframe.cloud.domain.posts.Posts;
 import org.egovframe.cloud.domain.posts.PostsRepository;
+import org.egovframe.cloud.web.dto.PostsListResponseDto;
 import org.egovframe.cloud.web.dto.PostsResponseDto;
 import org.egovframe.cloud.web.dto.PostsSaveRequestDto;
 import org.egovframe.cloud.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,11 +34,27 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        // 존재하는 Posts인지 확인 후 처리
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // Posts의 Stream을 map을 통해 PostsListResponseDto로 변환한다. 실제로 .map(posts -> new PostsListResponseDto(posts)) 과 같다.
+                .collect(Collectors.toList());
     }
 
 }
