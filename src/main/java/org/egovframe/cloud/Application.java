7ca259e867a3bccf18815769c0e4e0ba4eedeb2e
kimjaeyeol <godbasic97@gmail.com>
2021-05-26 14:12:38 +0900

    swagger 설정

--- src/main/java/org/egovframe/cloud/Application.java
+++ src/main/java/org/egovframe/cloud/Application.java
@@ -2,7 +2,6 @@ package org.egovframe.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing JPA Auditing 활성화 삭제하고 JpaConfig 클래스 생성하여 분리함 - WebMvcTest 스캔 대상에서 제외하기 위해
 @SpringBootApplication
