ea8196fa609328ec800c61d509901595c1ebf8d7
kimjaeyeol <godbasic97@gmail.com>
2021-06-01 17:32:43 +0900

    전체 목록 api 추가

--- src/main/java/org/egovframe/cloud/web/PostsApiController.java
+++ src/main/java/org/egovframe/cloud/web/PostsApiController.java
@@ -2,10 +2,13 @@ package org.egovframe.cloud.web;
 
 import lombok.RequiredArgsConstructor;
 import org.egovframe.cloud.service.PostsService;
+import org.egovframe.cloud.web.dto.PostsListResponseDto;
 import org.egovframe.cloud.web.dto.PostsResponseDto;
 import org.egovframe.cloud.web.dto.PostsSaveRequestDto;
 import org.egovframe.cloud.web.dto.PostsUpdateRequestDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성하여, 빈을 생성자로 주입받게 한다.
 @RestController
@@ -27,6 +30,11 @@ public class PostsApiController {
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
+    }
+
+    @GetMapping("/api/v1/posts")
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsService.findAllDesc();
     }
 
     @GetMapping("/api/v1/posts/{id}")
